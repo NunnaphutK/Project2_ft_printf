3f011acea60322cdb2b50c6cbc448c427bcf5572
Nunnaphut Kiticha <nkiticha@z1t5c3.42bangkok.com>
2023-11-26 18:09:11 +0700

    printf v6

--- ft_printf_di.c
+++ ft_printf_di.c
@@ -6,7 +6,7 @@
 /*   By: nkiticha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 13:11:21 by nkiticha          #+#    #+#             */
-/*   Updated: 2023/11/26 17:59:53 by nkiticha         ###   ########.fr       */
+/*   Updated: 2023/11/26 18:07:49 by nkiticha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@ static int	ft_p_di(int n)
 	char	x;
 
 	if (n < 0 && n > -10)
-		write(1, "-", 1);
+	{
+		if (write(1, "-", 1) == -1)
+			return (-1);
+	}
 	else if (n > 9 || n < -9)
-		ft_p_di(n / 10);
+	{
+		if (ft_p_di(n / 10) == -1)
+			return (-1);
+	}
 	n = n % 10;
 	if (n < 0)
 		n = -n;
